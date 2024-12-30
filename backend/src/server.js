493afa9a46da7961d67e46ea0422a20e38c6ebb3
Lussskki <lussskki@gmail.com>
2024-12-30 12:46:35 +0400

    testing api

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,16 @@
+// Import express for server handling
 import express from 'express'
+// Import body parser for handling json files
+import bodyParser from 'body-parser'
+// Import dotenv for security
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
+
+app.get('/', (req,res) => {
+    console.log('Api running')
+})
 
 app.listen(3000, () => {
     console.log('Server listens on http://localhost:3000')
