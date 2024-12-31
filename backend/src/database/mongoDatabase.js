63d65af9eec8720b08b0b0775a3aae956d9bcaad
Luka Guledani <lukaguledani123@gmail.com>
2024-12-31 13:48:55 +0400

    connection to database

---
+++ backend/src/database/mongoDatabase.js
@@ -0,0 +1,15 @@
+import mongosee from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+// Connection string of database
+const connectionString = process.env.CONNECTION_STRING
+
+try{
+    mongosee.connect(connectionString)
+    console.log('MongoDB.js connected successfully to database')
+}catch(error){
+    console.log('MongoDb.js Have error: ', error)
+}
+
+export default mongosee
